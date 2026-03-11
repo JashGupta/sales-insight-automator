35fb0030e04fd2c2a7956e1f2e1e6b5855661c44
LAKSHYA <lakshya@LAKSHYAs-MacBook-Air.local>
2026-03-11 18:52:08 +0530

    fixed email issue

--- backend/email.js
+++ backend/email.js
@@ -1,25 +1,20 @@
 const nodemailer = require("nodemailer")
 
 const transporter = nodemailer.createTransport({
-
- service:"gmail",
-
- auth:{
-  user:process.env.EMAIL_USER,
-  pass:process.env.EMAIL_PASS
- }
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 })
 
-module.exports = async (to,summary)=>{
-
- await transporter.sendMail({
-
-  from:process.env.EMAIL_USER,
-  to,
-  subject:"Sales Insight Report",
-  text:summary
-
- })
-
+module.exports = async (to, summary) => {
+  await transporter.sendMail({
+    from: process.env.EMAIL_USER,
+    to: to,
+    subject: "Sales Insight Report",
+    text: summary
+  })
 }
