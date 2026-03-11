cbe5aef71b7d78ffc01c875ffa94249233163f2e
LAKSHYA <lakshya@LAKSHYAs-MacBook-Air.local>
2026-03-11 19:28:28 +0530

    fixed email issues

--- backend/email.js
+++ backend/email.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY)
 module.exports = async (to, summary) => {
   try {
     await resend.emails.send({
-      from: "onboarding@resend.dev",
+      from: "jashgupta77@gmail.com",
       to: to,
       subject: "Sales Insight Report",
       text: summary
